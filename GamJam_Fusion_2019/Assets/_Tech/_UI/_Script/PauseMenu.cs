b5d0891ab991b9da1dcbd7fd21d7bae7400b3a37
nguyenhanson1 <nguyenhanson1@gmail.com>
2019-03-29 23:20:04 -0700

    pause secene wip

---
+++ GamJam_Fusion_2019/Assets/_Tech/_UI/_Script/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public KeyCode pauseKey;
+    private bool paused = false;
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void onEnabled()
+    {
+        UpdateHandler.UpdateOccurred += checkPauseMenu;
+    }
+    void onDisabled()
+    {
+        UpdateHandler.UpdateOccurred -= checkPauseMenu;
+    }
+
+    // Update is called once per frame
+    void checkPauseMenu()
+    {
+        if (Input.GetKey(pauseKey))
+        {
+            if(paused)
+            {
+                resume();
+            }
+            else
+            {
+                pause();
+            }
+        }
+    }
+    private void resume()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+    private void pause()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+}
