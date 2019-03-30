1089a570a22839a06c9bb9c341317423cf69f689
jusin-PC\Just <justincheung6@gmail.com>
2019-03-30 16:05:41 -0700

    Script bit of toaster is done

---
+++ GamJam_Fusion_2019/Assets/_Tech/_UI/_Script/Toaster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toaster : MonoBehaviour
+{
+    private Animator animt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animt = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(animt.GetCurrentAnimatorStateInfo(0).IsName("Oven Mehigy")){
+            Destroy(gameObject);
+        }
+    }
+}
