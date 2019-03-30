79193f8c54806192ee2f6b4d580b4ab326306020
Alex Pinedo <31834217+rocketmanhs@users.noreply.github.com>
2019-03-30 09:48:10 -0700

    Animations working

--- GamJam_Fusion_2019/Assets/_Tech/_Scripts/_InteractableObjects/Fridge.cs
+++ GamJam_Fusion_2019/Assets/_Tech/_Scripts/_InteractableObjects/Fridge.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Fridge : OpenCloseDoors
 {
+    [SerializeField] private Animator FridgeDoor;
+    
     public override void OnOpenDoor() {
         base.OnOpenDoor();
         Debug.Log("Fridge Opened");
+        FridgeDoor.SetBool("doorOpened", true);
+        
     }
 
     public override void OnCloseDoor()
     {
         base.OnCloseDoor();
         Debug.Log("Fridge Closed");
+        FridgeDoor.SetBool("doorOpened", false);
+
     }
 }
