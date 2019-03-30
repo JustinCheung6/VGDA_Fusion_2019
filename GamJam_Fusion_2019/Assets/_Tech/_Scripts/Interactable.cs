a0495505ffb7d759bd35b36cedddd8ecc957e2ea
Jessica Wei <jessicawei9@gmail.com>
2019-03-29 21:53:18 -0700

    input updated -- starting with interactables

---
+++ GamJam_Fusion_2019/Assets/_Tech/_Scripts/Interactable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject interact;
+
+    public float bufferDistance = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("LeftClick")) {
+            Debug.Log("Left mouse click");
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Right mouse click");
+        }
+        if (Input.GetMouseButtonDown(2))
+        {
+            Debug.Log("Right mouse click");
+        }
+    }
+}
