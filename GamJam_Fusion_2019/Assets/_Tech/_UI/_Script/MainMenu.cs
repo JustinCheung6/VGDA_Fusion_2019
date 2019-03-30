fda1501d52c603fec6ebe237e08e39be9f40860e
jusin-PC\Just <justincheung6@gmail.com>
2019-03-30 12:38:02 -0700

    Got Sounds for Main Menu to work

--- GamJam_Fusion_2019/Assets/_Tech/_UI/_Script/MainMenu.cs
+++ GamJam_Fusion_2019/Assets/_Tech/_UI/_Script/MainMenu.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    private AudioSource bsm;
+
+    private void Start()
+    {
+        bsm = GetComponent<AudioSource>();
+    }
+
     public void quitGame()
     {
+        Debug.Log("Talk to me");
+        bsm.Play();
         Application.Quit();
     }
     public void playGame()
     {
-        
+        Debug.Log("Talk to me");
+        bsm.Play();
     }
 }
