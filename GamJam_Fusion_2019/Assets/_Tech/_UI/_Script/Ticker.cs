2668d5b03eedaaa4b57376f9f820ed963cf8bd2b
nguyenhanson1 <nguyenhanson1@gmail.com>
2019-03-29 22:21:10 -0700

    finised timer bar UI

---
+++ GamJam_Fusion_2019/Assets/_Tech/_UI/_Script/Ticker.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Ticker : MonoBehaviour
+{
+    public Image currentTicker;
+    public Text ratioText;
+
+    private float timeLeft = 0;
+    private float maxTime = 10;
+
+    //eat my candy ass
+
+    private void Start()
+    {
+        currentTicker = GetComponent<Image>();
+    }
+    private void Update()
+    {
+        if (timeLeft < maxTime)
+        {
+            incrementTicker(Time.deltaTime);
+        }
+        else
+        {
+            Time.timeScale = 0;
+        }
+    }
+    private void updateTicker()
+    {
+        float ratio = timeLeft / maxTime;
+        currentTicker.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        ratioText.text = (ratio*100).ToString("0") + '%';
+    }
+
+    private void incrementTicker(float value)
+    {
+        timeLeft += value;
+        if (timeLeft > maxTime)
+        {
+            timeLeft = maxTime;
+        }
+        updateTicker();
+    }
+
+    private void decrementTicker(float value)
+    {
+        timeLeft -= value;
+        if (timeLeft < 0)
+        {
+            timeLeft = 0;
+            Debug.Log("dead");
+        }
+        updateTicker();
+    }
+}
