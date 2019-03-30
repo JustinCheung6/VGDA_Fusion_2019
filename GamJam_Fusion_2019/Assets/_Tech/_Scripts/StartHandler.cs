87a619cde4977fea2552675c3c979d38cc269ee7
Jessica Wei <jessicawei9@gmail.com>
2019-03-29 22:35:51 -0700

    start handler made

---
+++ GamJam_Fusion_2019/Assets/_Tech/_Scripts/StartHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartHandler : MonoBehaviour
+{
+    public delegate void onStart();
+
+    public static event onStart StartOccurred;
+
+    public void Start()
+    {
+        if (StartOccurred != null) 
+            StartOccurred();
+    }
+}
